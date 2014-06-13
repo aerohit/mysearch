da28496dd0f60e9791e80d779f449d027c1f97e0
Rohit Saxena <aerohit.saxena@gmail.com>
2014-06-14 00:16:10 +0200

    Fixed broken condition

--- app/games/FifteenStrikes.scala
+++ app/games/FifteenStrikes.scala
@@ -1,5 +1,9 @@
 package games
 
-case class FifteenStrikes(player1: Player, player2: Player, sticks: Int = 15)
+case class FifteenStrikes(player1: Player, player2: Player, sticks: Int = 15) {
+  if(player1.id == player2.id) throw InvalidRequestException("Player should have distinct ids")
+}
 
 case class Player(id: Int)
+
+case class InvalidRequestException(message: String) extends Exception
--- test/games/FifteenStrikesTest.scala
+++ test/games/FifteenStrikesTest.scala
@@ -4,11 +4,15 @@ import org.specs2.mutable.Specification
 
 class FifteenStrikesTest extends Specification {
   "When the game is started, it" should {
-    "have two players with distinct ids" in {
+    "have two players" in {
       val game = FifteenStrikes(Player(15), Player(17))
       game.player1.id mustEqual 15
       game.player2.id mustEqual 17
       game.player1.id mustNotEqual game.player2.id
+    }
+
+    "have distinct player ids" in {
+      FifteenStrikes(Player(15), Player(15)) must throwAn(InvalidRequestException("Player should have distinct ids"))
     }
 
     "have 15 sticks" in {
