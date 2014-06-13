2f1f51e17d880ad1b4773c12de17eb6d373da991
Rohit Saxena <aerohit.saxena@gmail.com>
2014-06-14 00:28:57 +0200

    A player could only strike 1, 2 or 3 sticks

--- app/games/FifteenStrikes.scala
+++ app/games/FifteenStrikes.scala
@@ -1,7 +1,12 @@
 package games
 
-case class FifteenStrikes(player1: Player, player2: Player, sticks: Int = 15) {
-  if(player1.id == player2.id) throw InvalidRequestException("Player should have distinct ids")
+case class FifteenStrikes(player1: Player, player2: Player, var sticks: Int = 15) {
+  if (player1.id == player2.id) throw InvalidRequestException("Player should have distinct ids")
+
+  def strike(player: Player, howMany: Int): Unit = {
+    if (howMany < 1 || howMany > 3) throw InvalidRequestException("Must strike either 1, 2 or 3")
+    sticks -= howMany
+  }
 }
 
 case class Player(id: Int)
--- test/games/FifteenStrikesTest.scala
+++ test/games/FifteenStrikesTest.scala
@@ -20,4 +20,27 @@ class FifteenStrikesTest extends Specification {
       game.sticks mustEqual 15
     }
   }
+
+  "When a player makes a move, she" should {
+    val p1 = Player(1)
+    val p2 = Player(2)
+    val game = FifteenStrikes(p1, p2)
+
+    "not be allowed to strike less than 1 stick" in {
+      game.strike(p1, 0) must throwAn(InvalidRequestException("Must strike either 1, 2 or 3"))
+    }
+
+    "not be allowed to strike more than 3 sticks" in {
+      game.strike(p1, 4) must throwAn(InvalidRequestException("Must strike either 1, 2 or 3"))
+    }
+
+    "be able to strike 1, 2 or 3 stick" in {
+      game.strike(p1, 1)
+      game.sticks mustEqual 14
+      game.strike(p1, 2)
+      game.sticks mustEqual 12
+      game.strike(p1, 3)
+      game.sticks mustEqual 9
+    }
+  }
 }
