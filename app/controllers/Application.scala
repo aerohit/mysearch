29ec3557015eebcd0bfda87ec71a6929241f19ba
Rohit Saxena <aerohit.saxena@gmail.com>
2014-06-20 23:35:31 +0200

    Added a websocket based endpoint to get fortune cookies

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,8 +1,10 @@
 package controllers
 
+import play.api.libs.iteratee.{Iteratee, Concurrent}
 import play.api.mvc._
 import utils.FortuneTeller
 import utils.actions.LoggingAction
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
@@ -20,5 +22,15 @@ object Application extends Controller {
 
   def test = LoggingAction {
     Ok(views.html.test())
+  }
+
+  def fortunesocket = WebSocket.using[String] { request =>
+    val (out, channel) = Concurrent.broadcast[String]
+
+    val in = Iteratee.foreach[String] {
+      msg => println(msg)
+        channel push FortuneTeller.fortune()
+    }
+    (in, out)
   }
 }
