bcb535288b44a3ffe6da77fa5c50c6de8a2753e5
Rohit Saxena <aerohit.saxena@gmail.com>
2014-06-24 23:36:56 +0200

    Basic logic for player registration and starting a new game.
    For now the logic is handled by a GamesManager.

--- app/games/GamesManager.scala
+++ app/games/GamesManager.scala
@@ -1,8 +1,35 @@
 package games
 
-/**
- * Created by aerohit on 6/24/14.
- */
+import play.api.libs.json.JsString
+
+import scala.collection.mutable
+import scalaz.{\/-, -\/, \/}
+
 class GamesManager {
+  val players = mutable.ListBuffer[Player]()
+
+  def registerNewPlayer(player: Player) = {
+    players += player
+    player.channel push JsString(s"Player ${player.id} registered")
+  }
+
+  def startNewGame(): String \/ FifteenStrikes = {
+    if (players.size < 2)
+      -\/("Only single player registered")
+    else {
+      val player1 = players(0)
+      val player2 = players(1)
+      player1.channel push JsString("Game started, make a move")
+      player2.channel push JsString("Game started, waiting for player 14 to make move")
+      \/-(FifteenStrikes(player1, player2))
+    }
+  }
 
 }
+
+object GamesManager {
+  def apply(): GamesManager = {
+    new GamesManager
+  }
+}
+
--- test/games/GamesManagerTest.scala
+++ test/games/GamesManagerTest.scala
@@ -1,8 +1,42 @@
 package games
 
-/**
- * Created by aerohit on 6/24/14.
- */
-class GamesManagerTest {
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+import play.api.libs.iteratee.Concurrent.Channel
+import play.api.libs.json.{JsString, JsValue}
 
+import scalaz.{\/-, -\/}
+
+class GamesManagerTest extends Specification with Mockito {
+  "Games manager" should {
+    "register and acknowledge a new player" in {
+      val player = playerWithId(12)
+      val gamesManager = GamesManager()
+      gamesManager.registerNewPlayer(player)
+      there was one(player.channel).push(JsString("Player 12 registered"))
+    }
+
+    "should not allow a new game when only one player registered" in {
+      val player = playerWithId(12)
+      val gamesManager = GamesManager()
+      gamesManager.registerNewPlayer(player)
+      gamesManager.startNewGame() mustEqual -\/("Only single player registered")
+    }
+
+    "should allow to start new game when two players registered" in {
+      val player1 = playerWithId(12)
+      val player2 = playerWithId(14)
+      val gamesManager = GamesManager()
+      gamesManager.registerNewPlayer(player1)
+      gamesManager.registerNewPlayer(player2)
+      gamesManager.startNewGame() mustEqual \/-(FifteenStrikes(player1, player2))
+      there was one(player1.channel).push(JsString("Game started, make a move"))
+      there was one(player2.channel).push(JsString("Game started, waiting for player 14 to make move"))
+    }
+  }
+
+  private def playerWithId(id: Int): Player = {
+    val channel = mock[Channel[JsValue]]
+    Player(id, channel)
+  }
 }
