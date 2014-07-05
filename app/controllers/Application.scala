c1073d029f92eebf0d8ec5705b7aac0f41c1e009
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-06 00:08:06 +0200

    Playing with sockets

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,9 +1,14 @@
 package controllers
 
-import play.api.libs.json.{Json, JsString}
+import play.api.libs.Comet
+import play.api.libs.iteratee.{Enumeratee, Enumerator}
+import play.api.libs.json.Json
 import play.api.mvc._
+import play.twirl.api.Html
 import utils.FortuneTeller
 import utils.actions.LoggingAction
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
@@ -28,5 +33,26 @@ object Application extends Controller {
 
   def test = LoggingAction {
     Ok(views.html.test())
+  }
+
+  def chunkeddata = LoggingAction {
+    val events = Enumerator("foo\n", "bar\n", "baz\n").andThen(Enumerator.eof)
+    Ok.chunked(events)
+  }
+
+  def comet = LoggingAction {
+    val events = Enumerator("kiki", "foo", "bar")
+    // Ok.chunked(events &> Comet(callback = "console.log"))
+    Ok.chunked(events &> toCometMessage)
+  }
+
+  def sinatracomet = Action {
+    val events = Enumerator("kiki", "foo", "bar")
+    Ok.chunked(events &> Comet(callback = "parent.cometMessage"))
+  }
+
+  val toCometMessage = Enumeratee.map[String] { data =>
+    Thread.sleep(2000)
+    Html( """<script>console.log('""" + data + """')</script>""")
   }
 }
